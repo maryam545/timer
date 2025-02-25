e004cb1003b3285b241841780067f02ff507cc64
maryam <marychch19@gmail.com>
2025-02-25 21:31:46 +0330

    change color

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function timer() {
-  let sec = 0;
-  let timer = setInterval(function () {
-    document.querySelector(".timerDisplay").innerHTML = "00:" + sec;
-    sec++;
-    if (sec > 20) {
-      clearInterval(timer);
-    }
-  }, 1000);
-}
-window.onload = function () {
+const start = document.querySelector(".start");
+const container = document.querySelector("container");
+const circle = document.querySelector(".circle");
+start.addEventListener("click", function () {
+  circle.style.backgroundColor = "hsl(314, 23%, 52%)";
+  start.style.backgroundColor = "hsl(314, 23%, 52%)";
+  start.style.color = "black";
+  function timer() {
+    let sec = 0;
+    let timer = setInterval(function () {
+      document.querySelector(".timerDisplay").innerHTML = "00:" + sec;
+      sec++;
+      if (sec > 59) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
   timer();
-};
+});
