ed9cc472faf74c9dd0fa06f112da9cdf7334ac02
maryam <marychch19@gmail.com>
2025-02-26 15:14:22 +0330

    add

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 const start = document.querySelector(".start");
 const container = document.querySelector("container");
 const circle = document.querySelector(".circle");
-start.addEventListener("click", function () {
-  circle.style.backgroundColor = "hsl(314, 23%, 52%)";
-  start.style.backgroundColor = "hsl(314, 23%, 52%)";
-  start.style.color = "black";
-  function timer() {
-    let sec = 0;
-    let timer = setInterval(function () {
-      document.querySelector(".timerDisplay").innerHTML = "00:" + sec;
-      sec++;
-      if (sec > 59) {
-        clearInterval(timer);
-      }
-    }, 1000);
-  }
-  timer();
-});
+let input = document.querySelector(".input");
